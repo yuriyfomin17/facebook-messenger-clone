5c4447db9d44ab4ce95f26d0236ef3764024d4dd
yuriyfomin17 <fominyura2012@gmail.com>
2021-01-17 16:24:09 +0200

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
+//input field
+// send message button
+// messages themselves
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [input, setInput] = useState('')
+    return (
+        <div className="App">
+            <h1>Hello world</h1>
+            <input value={input} onChange={e=>setInput(e.target.value)}/>
+            <button> Send the message</button>
+        </div>
+    );
 }
 
 export default App;
